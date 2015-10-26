edc682ecf6154c3cdcb236897ca9d252a95e4741
Price Matthews <p0matt001@gmail.com>
2015-10-26 15:54:00 -0400

    Fixed bug with user profile validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ActiveRecord::Base
   validates :profile_name, presence: true,
                            uniqueness: true,
                            format: {
-                            with: /\A[a-zA-Z\-\_]+\Z/,
+                            with: /\A[a-zA-Z0-9_-]+\z/,
                             message: "Must be formatted correctly."
                            }
 
